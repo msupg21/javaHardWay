ac5e11918837ea11a30cae125af38f47e5c4236e
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-04-05 10:08:18 -0500

    test file 2

---
+++ TestFileTwo.java
@@ -0,0 +1,10 @@
+public class TestFileTwo {
+    public static void main(String[] args) {
+
+String info;
+info = "the second test";
+
+    System.out.println("this is another test");
+    System.out.println("This is " + info);
+  }
+}
