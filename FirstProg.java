764b12f794475cd2baa585d3ee1b7afabe1a212a
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-04-07 09:43:36 -0500

    exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+
+String myName = "Chris Bartkewicz";
+		System.out.println( "I " + myName + " am determined to learn how to code." );
+		System.out.println( "Today's date is April 7th" );
+	}
+}
