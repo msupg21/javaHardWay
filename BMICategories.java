bb9630c8a16eb2a011222d6c65b293139118e12a
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-04-24 17:14:18 -0500

    exercise 19, using mulitple if statements using if, else if, & else

--- BMICategories.java
+++ BMICategories.java
@@ -4,7 +4,8 @@ public class BMICategories {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     double m, lbs, kg, sqr, bmi;
-    int feet, inches, heightSum;
+    int feet, inches, heightSum, year;
+    String error ="You just dont get it!";
 
     System.out.print("Enter your feet: ");
     feet = keyboard.nextInt();
@@ -47,5 +48,32 @@ public class BMICategories {
     else {
     System.out.println( "very severely/\"morbidly\" obese" );
     }
+
+    System.out.print("What year were you born: ");
+    year = keyboard.nextInt();
+
+    if (year < 1980) {
+      System.out.println("You are old as dirt lol!");
+    }
+    else if (year <= 1980) {
+      System.out.println("You have live for 4 decades now.");
+    }
+    else if (year <= 1990) {
+      System.out.println("You have lived for 3 decades now.");
+    }
+    else if (year <= 2000) {
+      System.out.println("You have lived for 2 decades now");
+    }
+    else if (year <= 2010) {
+      System.out.println("You are still young boy!");
+    }
+    else if (year <= 2017) {
+      System.out.println("yea, you are a baby");
+    }
+    else {
+      System.out.println("You are not even born yet!");
+    }
+
+
   }
 }
