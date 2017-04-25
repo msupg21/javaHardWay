a6f1f95d7f9cd842e3c89dc6e6e9e70a9ac1b892
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-04-24 21:13:43 -0500

    exercise 20, learning to inmport an object (.System), & more if and else statements

--- CollegeAdmission.java
+++ CollegeAdmission.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CollegeAdmission {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    int math;
+    int math, income;
 
     out.println("Welcome to the UT Austin College Admissions Interface!");
     out.print("Please enter your SAT math score (200-800): ");
@@ -12,39 +12,31 @@ public class CollegeAdmission {
 
     out.print("Admittance status: ");
 
-    if (math >= 790) {
+    if (math >= 790)
       out.print("CERTAIN");
-    }
-
-    else {
-      if (math >= 710) {
-          out.print("SAFE");
-    }
-
-    else {
-      if (math >= 580) {
+    else if (math >= 710)
+      out.print("SAFE");
+    else if (math >= 580)
       out.print("PROBABLE");
-    }
-
-    else {
-      if (math >= 500) {
+    else if (math >= 500)
       out.print("UNCERTAIN");
-    }
-
-    else {
-      if (math >= 390) {
+    else if (math >= 390)
       out.print("UNLIKELY");
-    }
+    else  // below 390
+      out.println("DENIED");
+      out.println();
 
-    else { // below 390
-      out.print("DENIED");
+    out.println("What is your yearly income?  ");
+    income = keyboard.nextInt();
 
-
-              }
-          }
-        }
-      }
-    }
+    if (income >= 100000)
+      out.print("You are well off");
+    else if (income > 60000)
+      out.print("You are in the middle class");
+    else if (income > 30000)
+      out.print("You probably make ends meet.");
+    else
+      out.print("You are broke dude!");
       out.println();
   }
 }
