c2f3709e69e7334c809182371511b23d0391ea84
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-04-26 15:12:48 -0500

    exercise 25, using while loop to verify passwords and pins

--- EnterPin.java
+++ EnterPin.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class EnterPin {
     public static void main(String[] args) {
       Scanner keyboard = new Scanner(System.in);
-      int pin, entry;
+      int pin = 12345, entry;
+      String pass;
 
-      pin = 12345;
+      System.out.println("Enter password please: ");
+      pass = keyboard.next();
+
+      while (! pass.equals("hunter2")) {
+          System.out.println("\nIncorrect password. Try again.");
+          System.out.println("Enter password please: ");
+          pass = keyboard.next();
+      }
+
 
       System.out.println("Welcome to the bank of Java.");
       System.out.print("Enter your pin: ");
